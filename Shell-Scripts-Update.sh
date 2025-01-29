1982f5ed72807785134bd66179876f5b24e75552
Aarav <aaravshah0011@gmail.com>
2025-01-30 00:08:34 +0530

    Adding self update code

---
+++ Shell-Scripts-Update.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Adding Commit Message
+read -p "Add Git Commit Message: " gitcommsgs
+
+# Change directory
+cd ~/Desktop/Shell\ Scripts/
+
+# Adding all files to git
+git add .
+
+# Commiting the files and adding a message via clipboard
+git commit -m "$gitcommsg"
+
+# Pushing the repo to git
+git push -u origin main
